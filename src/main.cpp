3fe112a6ef44e05adbd81734eedde7b9de26e3d6
Sebastian Pape <sebastian.pape@rwth-aachen.de>
2026-01-25 17:05:52 +0100

    Hiding console window on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "MainWindow.h"
 #include <gtkmm/application.h>
-
+#ifdef _WIN32
+    // for __argc & __argv
+    #include <cstdlib>
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -8,3 +11,11 @@ int main(int argc, char* argv[])
     //Shows the window and returns when it is closed.
     return app->make_window_and_run<MainWindow>(argc, argv);
 }
+
+#ifdef _WIN32
+    //Distinction is need to not open console window on Windows alongside the UI
+    int WinMain(void* hInstance, void* hPrevInstance, char* argv, int nCmdShow)
+    {
+        return main(__argc, __argv);
+    }
+#endif
