d9cf386d4eb2e20282f7c676f98619534056e0d8
will wade <willwade@gmail.com>
2026-06-01 17:12:31 +0100

    cleanup: remove old C++ DasherCore-dependent files
    Remove files that relied on direct C++ DasherCore inheritance:
    - DasherController.h/.cpp (replaced by DasherBridge)
    - ButtonMapper.h/.cpp (will be re-implemented with CAPI key_event)
    - FakeInput.h (test input, will be re-implemented)
    - JoystickInput (SDL3 joystick, needs CAPI re-implementation)
    - SettingsMode, SettingsInputMapping, SettingsHelp, SettingsPageBase,
      DeviceSettingsProvider (replaced by SettingsSection with CAPI introspection)
    - EnumDropdown (merged into SyncedEnumDropdown)
    
    Keep PopoverBox and PopoverMenuButtonInfo as they're still used.

--- src/Preferences/DeviceSettingsProvider.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "gtkmm/grid.h"
-
-class DeviceSettingsProvider {
-public:
-    virtual bool FillInputDeviceSettings(Gtk::Grid* grid){return false;}
-};
--- src/UIComponents/PopoverMenuButtonInfo.h
+++ src/UIComponents/PopoverMenuButtonInfo.h
@@ -1,17 +1,13 @@
 #pragma once
 
-#include "glibmm/ustring.h"
-#include "gtkmm/label.h"
-#include "gtkmm/image.h"
-#include "gtkmm/popover.h"
-#include "gtkmm/eventcontrollermotion.h"
+#include <gtkmm/eventcontrollermotion.h>
+#include <gtkmm/image.h>
+#include <gtkmm/label.h>
+#include <gtkmm/popover.h>
 
-#pragma clang optimize off
-class PopoverMenuButtonInfo : public Gtk::Image
-{
-
+class PopoverMenuButtonInfo : public Gtk::Image {
 public:
-    PopoverMenuButtonInfo(Glib::ustring text) : m_label(text) {
+    PopoverMenuButtonInfo(const Glib::ustring& text) : m_label(text) {
         set_from_icon_name("help-about-symbolic");
         set_name("InfoPopup");
 
@@ -19,29 +15,23 @@ public:
         m_popover.set_child(m_label);
         m_popover.set_has_arrow(false);
         m_popover.set_autohide(false);
-        
-        // Open on mouse-over
-        m_mouseController = Gtk::EventControllerMotion::create();
-        m_mouseController->signal_enter().connect([this](double, double){
+
+        auto controller = Gtk::EventControllerMotion::create();
+        controller->signal_enter().connect([this](double, double) {
             m_popover.popup();
-            grab_focus(); //As focus is shifted to the popup, signal_leave is immediately executed. Grab focus and disable autohide to prevent this.
+            grab_focus();
         });
-        m_mouseController->signal_leave().connect([this](){
+        controller->signal_leave().connect([this]() {
             m_popover.popdown();
         });
-        add_controller(m_mouseController);
-    
-        signal_destroy().connect([this](){
+        add_controller(controller);
+
+        signal_destroy().connect([this]() {
             m_popover.unparent();
         });
     }
 
-    Glib::ustring GetText(){return m_label.get_text();}
-    void SetText(Glib::ustring text){return m_label.set_text(text);}
-
-protected:
+private:
     Gtk::Popover m_popover;
     Gtk::Label m_label;
-    Glib::RefPtr<Gtk::EventControllerMotion> m_mouseController;
 };
-#pragma clang optimize on
